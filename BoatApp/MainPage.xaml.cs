0c3766bf7fe0b5c1628dd919048af32b1960568c
Developer Digiture <developer.digiture@gmail.com>
2024-04-02 17:27:55 +0530

    initial

--- BoatApp/MainPage.xaml.cs
+++ BoatApp/MainPage.xaml.cs
@@ -16,12 +16,12 @@ namespace BoatApp
 
             appUIView = new AppUIView();
             appUIView.SourceUrl = AppConstants.LocaleLoaderPage;
-            //appUIView.ServiceType = "BoatService";
-            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/updateOne", ServiceName = "ConfirmDropRequest", RequestType = "ConfirmDropRequest" });
-            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "GetOwnerDetails", RequestType = "OwnerRequest" });
-            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "GetBoatsByOwner", RequestType = "BoatsRequest" });
-            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "SubmitDropRequest", RequestType = "SubmitDropRequest" });
-            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "GetAllRequests", RequestType = "GenericRequest" });
+            appUIView.ServiceType = "BoatService";
+            appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/updateOne", ServiceName = "ConfirmDropRequest", RequestType = "ConfirmDropRequest" });
+            appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "GetOwnerDetails", RequestType = "OwnerRequest" });
+            appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "GetBoatsByOwner", RequestType = "BoatsRequest" });
+            appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "SubmitDropRequest", RequestType = "SubmitDropRequest" });
+            appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "GetAllRequests", RequestType = "GenericRequest" });
             stk01.Add(appUIView);
         }
 
