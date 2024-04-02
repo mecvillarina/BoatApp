8ce7156ddaea162ef04d2ee52b4088e563754b88
Developer Digiture <developer.digiture@gmail.com>
2024-04-02 17:17:30 +0530

    initial

---
+++ BoatApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using NanoHandler;
+using NanoHandler.Configs;
+using BoatApp.Configs;
+
+
+
+namespace BoatApp
+{
+    public partial class MainPage : ContentPage
+    {
+        AppUIView appUIView;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            appUIView = new AppUIView();
+            appUIView.SourceUrl = AppConstants.LocaleLoaderPage;
+            //appUIView.ServiceType = "BoatService";
+            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/updateOne", ServiceName = "ConfirmDropRequest", RequestType = "ConfirmDropRequest" });
+            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "GetOwnerDetails", RequestType = "OwnerRequest" });
+            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "GetBoatsByOwner", RequestType = "BoatsRequest" });
+            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "SubmitDropRequest", RequestType = "SubmitDropRequest" });
+            //appUIView.Mapper.Add(new ERMapper() { Endpoint = "action/find", ServiceName = "GetAllRequests", RequestType = "GenericRequest" });
+            stk01.Add(appUIView);
+        }
+
+        
+    }
+
+}
---
+++ BoatApp/Requests/ConfirmDropRequest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BoatApp.Requests
+{
+    public class ConfirmDropRequest: APIRequest
+    {
+        public ConfirmDropFilter filter { get; set; }
+        public ConfirmDropUpdate update { get; set; }
+
+        public ConfirmDropRequest()
+        {
+            filter=new ConfirmDropFilter();
+            update=new ConfirmDropUpdate();
+        }
+    }
+
+    public class ConfirmDropFilter
+    {
+        public string boat_number { get; set; }
+    }
+
+    public class ConfirmDropUpdate
+    {
+        //[JsonProperty("$set")]
+        public ConfirmDropUpdateSet set { get; set; }
+        public ConfirmDropUpdate()
+        {
+            set = new ConfirmDropUpdateSet();
+        }
+    }
+
+    public class ConfirmDropUpdateSet
+    {
+        public string status { get; set; }
+    }
+}
