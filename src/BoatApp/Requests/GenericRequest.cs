b3f02d8e194074e99d090741f80e9cf940777c63
Mark Erwin Villarina <me.villarina@live.com>
2024-04-05 10:51:38 +0800

    Clean up

---
+++ src/BoatApp/Requests/GenericRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BoatApp.Requests
+{
+    public class GenericRequest : APIRequest
+    {
+        public GenericFilter filter { get; set; }
+
+        public GenericRequest()
+        {
+            filter=new GenericFilter();
+        }
+    }
+
+    public class GenericFilter
+    {
+
+    }
+
+}
