5070a3603f6803cd908202d31c97d4232f9ee852
Mark Erwin Villarina <me.villarina@live.com>
2024-04-05 16:51:02 +0800

    Update Login Page

--- src/BoatApp.Maui.UI/BoatApp.Maui.UI/Views/LoginPage.xaml.cs
+++ src/BoatApp.Maui.UI/BoatApp.Maui.UI/Views/LoginPage.xaml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BoatApp.Maui.UI.Views;
 
-namespace BoatApp.Maui.UI.Views;
-
-public partial class LoginPage : ContentPage
+public partial class LoginPage : PageBase
 {
     public LoginPage()
     {
