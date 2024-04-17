8cd5c8d5a5b53ae2127abdc1bf5d70d743c60b68
Mark Erwin Villarina <me.villarina@live.com>
2024-04-17 13:36:12 +0800

    Update PageBase

--- src/BoatApp.Maui.UI/Views/PageBase.cs
+++ src/BoatApp.Maui.UI/Views/PageBase.cs
@@ -1,8 +1,4 @@
 using BoatApp.Maui.UI.Helpers;
-using CommunityToolkit.Maui.Behaviors;
-using CommunityToolkit.Maui.Core;
-using Microsoft.Maui.Controls.PlatformConfiguration;
-using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
 
 namespace BoatApp.Maui.UI.Views;
 
@@ -11,14 +7,6 @@ public class PageBase : ContentPage
     public PageBase()
     {
         BackgroundColor = (Color)ResourceHelpers.FindResource("Background");
-        Microsoft.Maui.Controls.NavigationPage.SetHasNavigationBar(this, false);
-        
-        On<iOS>().SetUseSafeArea(false);
-        
-        this.Behaviors.Add(new StatusBarBehavior
-        {
-            StatusBarColor = (Color)ResourceHelpers.FindResource("Primary"),
-            StatusBarStyle = StatusBarStyle.LightContent
-        });
+        NavigationPage.SetHasNavigationBar(this, false);
     }
 }
