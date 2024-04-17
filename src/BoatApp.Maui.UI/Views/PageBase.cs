7f5b438ea29ad742d2c6c9f67c4f3bb9bbd37f1d
Mark Erwin Villarina <me.villarina@live.com>
2024-04-17 13:27:16 +0800

    Update Safe Area iOS

--- src/BoatApp.Maui.UI/Views/PageBase.cs
+++ src/BoatApp.Maui.UI/Views/PageBase.cs
@@ -1,4 +1,8 @@
 using BoatApp.Maui.UI.Helpers;
+using CommunityToolkit.Maui.Behaviors;
+using CommunityToolkit.Maui.Core;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
 
 namespace BoatApp.Maui.UI.Views;
 
@@ -7,6 +11,14 @@ public class PageBase : ContentPage
     public PageBase()
     {
         BackgroundColor = (Color)ResourceHelpers.FindResource("Background");
-        NavigationPage.SetHasNavigationBar(this, false);
+        Microsoft.Maui.Controls.NavigationPage.SetHasNavigationBar(this, false);
+        
+        On<iOS>().SetUseSafeArea(false);
+        
+        this.Behaviors.Add(new StatusBarBehavior
+        {
+            StatusBarColor = (Color)ResourceHelpers.FindResource("Primary"),
+            StatusBarStyle = StatusBarStyle.LightContent
+        });
     }
 }
